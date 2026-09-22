1910a57c0818996020e801eba00cb8bf66ee98db
benlo20270-web <benlo20270@gmail.com>
2026-09-22 13:18:07 -0700

    Add files via upload

---
+++ FormatDate.java
@@ -0,0 +1,10 @@
+public class FormatDate {
+	public static void printAmerican(String day, String month,  int date, int year){
+	System.out.print(day + " , " + month + " " + date + " , " + year);
+	}
+	public static void main(String[] args){
+	printAmerican("Monday", "November", 28, 2026);
+	}
+}
+
+
