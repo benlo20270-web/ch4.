af87faa767c24aa582ba4282fa2dd1e927ae9569
benlo20270-web <benlo20270@gmail.com>
2026-09-24 13:33:21 -0700

    Add files via upload

---
+++ Multadd.java
@@ -0,0 +1,30 @@
+public class Multadd {
+	public static double multadd(double a, double b, double c) {
+		double result =
+		a * b + c;
+		return result;
+}
+	
+	public static void  main (String[] args) {
+		double a = 1.0;
+		double b = 2.0;
+		double c = 3.0;
+		System.out.println(multadd(0.5, Math.cos(Math.PI / 4), 
+		Math.sin(Math.PI / 4)));
+		//log function is below
+		System.out.println(multadd(1, Math.log(10), 
+		Math.log(20)));
+		
+	
+public class Expsum {
+		public static double expsum(double x) {
+			double result = 
+			x * Math.pow(e , -x ) + Math.sqrt(1 - Math.pow(e, -x));
+			return result;
+		
+	}
+}
+
+}
+		
+
